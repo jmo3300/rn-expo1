273b40ceae502586660367819b5b7f2e6d38fa61
jmo3300 <jmo3300@gmx.net>
2022-06-17 06:54:41 +0200

    dotenv integrated

--- src/api/yelp.tsx
+++ src/api/yelp.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import { YELP_API_TOKEN } from '@env';
+
 export interface IBusiness {
   id: string,
   name: string,
@@ -10,7 +12,8 @@ export interface IBusiness {
 
 const yelp = () => {
 
-  const authorizationJWT = 'YePFgGv7JYVV8tt-wusxZTlnVDj7RNHLZGNpyRylyWo-HqWiyV9KOYKcscnBFH1sW-9JH8vH9YBHyGpMTGFOyBQv6IHfPU4MJQ5Ycw8kfcw3q4ZBhFICFuCXmWWfYnYx'
+  // const authorizationJWT = 'YePFgGv7JYVV8tt-wusxZTlnVDj7RNHLZGNpyRylyWo-HqWiyV9KOYKcscnBFH1sW-9JH8vH9YBHyGpMTGFOyBQv6IHfPU4MJQ5Ycw8kfcw3q4ZBhFICFuCXmWWfYnYx'
+  const authorizationJWT = YELP_API_TOKEN;
 
   return axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
---
+++ src/types/env.d.ts
@@ -0,0 +1,3 @@
+declare module '@env' {
+  export const YELP_API_TOKEN: string;
+}
